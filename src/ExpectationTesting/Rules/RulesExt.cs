387baeb5ac16ba9292e9e97b2969e3b34a2bf956
Denis Pimenov <pimenovdenis.prog@gmail.com>
2017-12-14 18:16:58 +0300

    Not completed on work

---
+++ src/ExpectationTesting/Rules/RulesExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
+
+namespace ExpectationTesting.Rules
+{
+    internal static class RulesExt
+    {
+        [CanBeNull]
+        internal static TProp GetPropertyValue<T,TProp>(this Expression<Func<T, TProp>> expression,T obj)
+        {
+            try
+            {
+                return expression.Compile().Invoke(obj);
+            }
+            catch (NullReferenceException)
+            {
+                return default(TProp);
+            }
+        }
+    }
+}
---
+++ test/ExpectationTesting.Specification/ConfigurableAssertionSpec.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+using ExpectationTesting.Rules;
+using ExpectationTesting.Specification.TestsObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace ExpectationTesting.Specification
+{
+    public class ConfigurableAssertionSpec
+    {
+
+        /*public class SimpleRule<T,TProp>:IRule<T>
+        {
+            private readonly Expression<Func<T, TProp>> propExpression;
+
+            public SimpleRule(Expression<Func<T,TProp>> propExpression ,Func<TProp>)
+            {
+                this.propExpression = propExpression;
+            }
+
+            public bool Assert(T original, T current)
+            {
+                var original
+            }
+        }*/
+
+        [Fact]
+        public void Assert_Should_Return_True_If_Havent_Rules()
+        {
+            var entity = new Entity();
+            var assertion = Except.That(entity);
+            assertion.Assert().Should().BeTrue();
+        }
+
+       /* [Fact]
+        public void Can_Add_Simple_Rule_Then_Assert()
+        {
+            var entity = new Entity();
+            var assertion = Except.That(entity);
+            assertion.AddRule(new SimpleRule<Entity,int>(entity1 => entity.Id));
+            assertion.Assert().Should().BeTrue();
+        }*/
+    }
+}
